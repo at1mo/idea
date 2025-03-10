e4671e615feb4d8d56d834df2683abc95a63fd2c
A.Timokhin <timoha.job@gmail.com>
2025-03-10 09:07:57 +0300

    feat: 23 chapter - 6, 7

--- backend/src/lib/logger.ts
+++ backend/src/lib/logger.ts
@@ -1,10 +1,12 @@
 import { EOL } from 'os';
+import debug from 'debug';
 import _ from 'lodash';
 import pc from 'picocolors';
 import { serializeError } from 'serialize-error';
 import { MESSAGE } from 'triple-beam';
 import winston from 'winston';
 import * as yaml from 'yaml';
+import { deepMap } from '../utils/deepMap';
 import { env } from './env';
 
 export const winstonLogger = winston.createLogger({
@@ -65,17 +67,43 @@ export const winstonLogger = winston.createLogger({
   ],
 });
 
+type Meta = Record<string, any> | undefined;
+const prettifyMeta = (meta: Meta): Meta => {
+  return deepMap(meta, ({ key, value }) => {
+    if (
+      [
+        'email',
+        'password',
+        'newPassword',
+        'oldPassword',
+        'token',
+        'text',
+        'description',
+      ].includes(key)
+    ) {
+      return '🙈';
+    }
+    return value;
+  });
+};
+
 export const logger = {
-  info: (logType: string, message: string, meta?: Record<string, any>) => {
-    winstonLogger.info(message, { logType, ...meta });
+  info: (logType: string, message: string, meta?: Meta) => {
+    if (!debug.enabled(`ideanick:${logType}`)) {
+      return;
+    }
+    winstonLogger.info(message, { logType, ...prettifyMeta(meta) });
   },
-  error: (logType: string, error: any, meta?: Record<string, any>) => {
+  error: (logType: string, error: any, meta?: Meta) => {
+    if (!debug.enabled(`ideanick:${logType}`)) {
+      return;
+    }
     const serializedError = serializeError(error);
     winstonLogger.error(serializedError.message || 'Unknown error', {
       logType,
       error,
       errorStack: serializedError.stack,
-      ...meta,
+      ...prettifyMeta(meta),
     });
   },
 };
