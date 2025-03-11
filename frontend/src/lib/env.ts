df554ea186579c2e1290e496c3571bad8ed855f0
A.Timokhin <timoha.job@gmail.com>
2025-03-11 22:01:48 +0300

    feat: 23 chapter - 12, 13, 14

--- frontend/src/lib/env.ts
+++ frontend/src/lib/env.ts
@@ -8,6 +8,7 @@ import { z } from 'zod';
 export const zEnv = z.object({
   NODE_ENV: z.enum(['development', 'production']),
   HOST_ENV: zEnvHost,
+  SOURCE_VERSION: zEnvNonemptyTrimmedRequiredOnNotLocal,
   VITE_BACKEND_TRPC_URL: zEnvNonemptyTrimmed,
   VITE_FRONTEND_URL: zEnvNonemptyTrimmed,
   VITE_FRONTEND_SENTRY_DSN: zEnvNonemptyTrimmedRequiredOnNotLocal,
