fed5c36edc153abe794fe750ee867a0118a0d587
A.Timokhin <timoha.job@gmail.com>
2025-03-12 21:22:14 +0300

    feat: 24 chapter - 2

--- backend/src/scripts/notifyAboutMostLikedIdeas.ts
+++ backend/src/scripts/notifyAboutMostLikedIdeas.ts
@@ -1,20 +1,31 @@
-import { type Idea } from '@prisma/client';
+import { Prisma, type Idea } from '@prisma/client';
 import { type AppContext } from '../lib/ctx';
 import { sendMostLikedIdeasEmail } from '../lib/emails';
 
-export const notifyAboutMostLikedIdeas = async (ctx: AppContext) => {
-  const mostLikedIdeas = await ctx.prisma.$queryRaw<
+export const getMostLikedIdeas = async (
+  ctx: AppContext,
+  limit: number = 10,
+  now?: Date
+) => {
+  const sqlNow = now
+    ? Prisma.sql`${now.toISOString()}::timestamp`
+    : Prisma.sql`now()`;
+  return await ctx.prisma.$queryRaw<
     Array<Pick<Idea, 'id' | 'nick' | 'name'> & { thisMonthLikesCount: number }>
   >`
-    select i.id, i.nick, i.name, count(il."ideaId")::int as "thisMonthLikesCount"
-      from "Idea" i
-        join "IdeaLike" il ON i.id = il."ideaId"
-        where il."createdAt" > now() - interval '1 month'
-        and i."blockedAt" is null
-        group by i.id, i.nick, i.name
-        order by "thisMonthLikesCount" desc
-        limit 10;
-  `;
+  select i.id, i.nick, i.name, count(il."ideaId")::int as "thisMonthLikesCount"
+    from "Idea" i
+      join "IdeaLike" il ON i.id = il."ideaId"
+      where il."createdAt" > ${sqlNow} - interval '1 month'
+      and i."blockedAt" is null
+      group by i.id, i.nick, i.name
+      order by "thisMonthLikesCount" desc
+      limit ${limit};
+`;
+};
+
+export const notifyAboutMostLikedIdeas = async (ctx: AppContext) => {
+  const mostLikedIdeas = await getMostLikedIdeas(ctx);
 
   if (!mostLikedIdeas.length) {
     return;
